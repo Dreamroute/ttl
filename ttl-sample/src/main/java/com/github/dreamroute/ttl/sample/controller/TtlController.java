453af00033479c503ced97bc9a3a84b099fc1dad
w.dehai <342252328@qq.com>
2021-03-03 18:04:33 +0800

    build: 加入构建相关依赖

--- ttl-sample/src/main/java/com/github/dreamroute/ttl/sample/controller/TtlController.java
+++ ttl-sample/src/main/java/com/github/dreamroute/ttl/sample/controller/TtlController.java
@@ -1,11 +1,14 @@
 package com.github.dreamroute.ttl.sample.controller;
 
+import com.github.dreamroute.ttl.sample.service.AsyncService;
 import com.github.dreamroute.ttl.sample.service.PoolUseService;
 import com.github.dreamroute.ttl.sample.service.TtlService;
-import com.github.dreamroute.ttl.sample.service.AsyncService;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.alibaba.fastjson.JSON.toJSONString;
 
 @RestController
 @AllArgsConstructor
@@ -16,10 +19,17 @@ public class TtlController {
     private PoolUseService poolUseService;
 
     @GetMapping("/ttl")
-    public String ttl() {
+    public String ttl(Long id) {
         ttlService.ttl();
         asyncService.as();
         poolUseService.pu();
-        return "ok";
+        Dev dev = new Dev();
+        dev.setId(id);
+        return toJSONString(dev);
     }
+}
+
+@Data
+class Dev {
+    private Long id;
 }
